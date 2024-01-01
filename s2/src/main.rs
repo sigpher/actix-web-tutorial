e201b2a87b81cc4f2c3f0fcd3b5256a6b653ee9c
sigpher <425223341@qq.com>
2024-01-01 20:28:45 +0800

    before jwt

--- s2/src/main.rs
+++ s2/src/main.rs
@@ -1,15 +1,27 @@
 use std::io;
 
-use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
+use actix_web::{
+    get,
+    middleware::{self, Logger},
+    post, web, App, HttpResponse, HttpServer, Responder,
+};
+use env_logger::Env;
+use serde::{Deserialize, Serialize};
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Compress::default())
+            .wrap(Logger::default())
+            // .wrap(Logger::new("%t %a"))
             .service(index)
             .service(product)
             .service(query_info)
+            .service(submit)
+            .service(form_submit)
+            .service(login)
             .route("/goods/{id}", web::get().to(goods))
     })
     .bind("127.0.0.1:8080")?
@@ -37,10 +49,37 @@ async fn product(product: web::Path<Product>) -> impl Responder {
     HttpResponse::Ok().body(format!("Product Category: {} name: {}", p.category, p.name))
 }
 
+// Query
 #[get("/query")]
 async fn query_info(info: web::Query<Info>) -> impl Responder {
     let info = info.into_inner();
     HttpResponse::Ok().body(format!("Username :{}", info.username))
+}
+
+#[post("/submit")]
+// Json
+async fn submit(info: web::Json<Info>) -> impl Responder {
+    HttpResponse::Ok().body(format!(
+        "Username :{}, Age: {}",
+        info.username,
+        info.age.unwrap()
+    ))
+}
+
+#[post("/form_submit")]
+async fn form_submit(form: web::Form<FormInfo>) -> impl Responder {
+    let form = form.into_inner();
+
+    HttpResponse::Ok().body(format!("{}: {}", form.username, form.password))
+}
+
+#[post("/login")]
+async fn login(user: web::Form<User>) -> io::Result<impl Responder> {
+    let user = User {
+        id: user.id,
+        username: user.username.to_string(),
+    };
+    Ok(web::Json(user))
 }
 
 #[derive(Deserialize)]
@@ -51,5 +90,18 @@ struct Product {
 
 #[derive(Deserialize)]
 struct Info {
+    username: String,
+    age: Option<u8>,
+}
+
+#[derive(Deserialize)]
+struct FormInfo {
+    username: String,
+    password: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: u32,
     username: String,
 }
